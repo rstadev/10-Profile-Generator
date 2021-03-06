cd7d0bcdbcf45ecc047406190c2d2281be9f6a90
unknown <nukasine@gmail.com>
2021-03-06 14:39:45 -0500

    added empty functions to index.js, as they are required but Im not sure how exactly these will work in my program yet

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ function Employee(name, id, email) {
     this.name = name;
     this.id = id;
     this.email = email;
+    this.getName = () => {
+
+    }
+    this.getId = () => {
+        
+    }
+    this.getEmail= () => {
+        
+    }
+    this.getRole = () => {
+        
+    }
 };
 
 
 const robert = new Employee('robert', 1, 'robert@place.net');
 
-console.log(robert)
+console.log(robert);
