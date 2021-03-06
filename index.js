f2bea8b889ed9ad528da018c98b48a585d6c94f2
unknown <nukasine@gmail.com>
2021-03-06 14:28:25 -0500

    added some function to my constructor and tested it to make sure it works. Trying to wrap my head around all the requirements detailed in the readme.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const jest = require('jest');
 const inquirer = require('inquirer');
-function Employee(name, id, email) {
 
+function Employee(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
 };
+
+
+const robert = new Employee('robert', 1, 'robert@place.net');
+
+console.log(robert)
