e7beb5d3f9e9fdd2e91345a4935c66e4265bccf6
unknown <nukasine@gmail.com>
2021-03-08 14:29:56 -0500

    got a test to pass on the first function to Employee. getName is now confirmed to post.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,15 @@
 const Employee = require('../lib/Employee');
 
 describe('Employee', () => {
+    describe('getName', () => {
+        it('should return the Employee"s name when called', () => {
+            const obj = new Employee('jacob', 2, 'jacob@jacobplace.net');
+            
+            console.log(obj.name);
+
+            expect(obj.name).toEqual('jacob');
+
+        });
+    });
 
 });
