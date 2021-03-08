41ebc9b29f11455b76e7452a0776d2d4129246c3
unknown <nukasine@gmail.com>
2021-03-08 14:51:21 -0500

    added test for second function in employee. Confirmed that it works as well.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,14 +1,25 @@
 const Employee = require('../lib/Employee');
 
+
+const obj = new Employee('jacob', 2, 'jacob@jacobplace.net');
+
 describe('Employee', () => {
     describe('getName', () => {
         it('should return the Employee"s name when called', () => {
-            const obj = new Employee('jacob', 2, 'jacob@jacobplace.net');
             
-            console.log(obj.name);
+            console.log(`Returned ${obj.name}`);
 
             expect(obj.name).toEqual('jacob');
 
+        });
+    });
+
+    describe('getId', () => {
+        it('should return the Employee"s id number when called', () => {
+
+            console.log(`Returned ${obj.id}`)
+
+            expect(obj.id).toEqual(2);
         });
     });
 
