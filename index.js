69b50461afcc6483d2d06f649c37359f3ba6baf9
unknown <nukasine@gmail.com>
2021-03-08 14:07:34 -0500

    added a gitignore file to the repo so that it will ignore the node_modules file. Also beginning to set up the framework for the tests for the employee constructor.

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const jest = require('jest');
 const inquirer = require('inquirer');
 const Employee = require('./lib/Employee.js')
 
-// Employee.prototype.getName = () => {
 
-// }
 
 const robert = new Employee('robert', 1, 'robert@place.net');
+
 
 console.log(robert);
