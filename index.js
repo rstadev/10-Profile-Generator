69abd5274fa0314bea08e1a77013eca863aa8fdb
unknown <nukasine@gmail.com>
2021-03-21 13:19:53 -0400

    placed a quesiton into a questions array and introduced inquirer to the app. Im not sure if I will keep the questions array seperate from inquirer for the rest of the project

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-const jest = require('jest');
+// const jest = require('jest');
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee.js')
+const Employee = require('./lib/Employee.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+const Manager = require('./lib/Manager.js');
+
+const questions = [
+  {
+    type: 'input',
+    name:'Mname',
+    message:'What is your Team Manager"s name?',
+  },
 
 
 
-const robert = new Employee('robert', 1, 'robert@place.net');
+];
 
 
-console.log(robert);
+
+
+writeToFile = (fileName, data) => {
+    fs.writeFile(fileName,  data, (err) => 
+    err ? console.error(err) : console.log("Success!")
+  )};
+
+function init() {
+  inquirer.prompt(questions).then(answers => {
+    console.log(answers)
+  })
+}
+
+init();
+
+
+// console.log(robert);
+// const robert = new Employee('robert', 1, 'robert@place.net');
